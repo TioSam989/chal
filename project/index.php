3ff244ec40dbcf341339db5bfec2e710162b7e73
Davi Neves <tio.sam989@hotmail.com>
2023-11-10 05:24:37 +0000

    changed name and fixed missing char

--- project/index.php
+++ project/index.php
@@ -8,25 +8,34 @@ $bookController = new BookController($db);
 $requestedPage = $_SERVER['REQUEST_URI'];
 
 switch ($requestedPage) {
+
     case '/chal/project/': //home page
         include 'app/views/home.php';
         break;
+
     case '/chal/project/books': //show all books
         $bookController->index();
         break;
+
     case '/chal/project/books/add': //add book page
         include 'app/views/books/create.php';
         break;
+
     case '/chal/project/books/storeBook': //add book function
         $bookController->store();
         break;
-    case 'chal/project/books/show':
-        $book_id = $_GET['book_id'];
-        include 'app/views/books/show.php';
+
+    case '/chal/project/books/modify': //add edit page
+        include 'app/views/books/edit.php';
         break;
+
+    case '/chal/project/books/updateData':
+        $bookController->edit();
+
     case '/chal/project/books/delete':
         $bookController->delete();
         break;
+
     default:
         include('app/views/404.php');
         break;

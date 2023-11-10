8c124fd6e102967950f0292e2aaf5717212a7a25
Davi Neves <tio.sam989@hotmail.com>
2023-11-10 01:47:36 +0000

    separating heade footer  and head to use well

---
+++ project/app/views/footer.php
@@ -0,0 +1,3 @@
+<footer>
+book place
+</footer>
---
+++ project/app/views/header.php
@@ -0,0 +1,14 @@
+<header>
+
+    <h2><span class="mainColor">M</span>y <span class="mainColor">O</span>wn <span class="mainColor">B</span>ooks</h2>
+    <h4>The perfect place to organize your books</h4>
+
+    <nav>
+        <a href="<?php echo BASE_URL;?>">Home</a>
+        <a href="<?php echo BASE_URL;?>books">All Books</a>
+        <a href="<?php echo BASE_URL;?>books/add">Add Book</a>
+    </nav>
+
+
+</header>
+

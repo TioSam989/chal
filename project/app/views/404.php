f7cd3622167de3b4abaf9ed7a9584ce433d07dc3
Davi Neves <tio.sam989@hotmail.com>
2023-11-10 01:46:21 +0000

    set up simple 404 page with btn

---
+++ project/app/views/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('head.php'); ?>
+    <title>Not Found</title>
+</head>
+
+<body>
+    <section>
+        <a href="<?php echo BASE_URL; ?>"><button >Go Back to App</button></a>
+        <p>Not Found</p>
+    </section>
+</body>
+
+</html>
